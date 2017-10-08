d61689ff7976734a8689308dce30f58624e12a55
Alexey <doublein@rambler.ru>
2017-10-08 13:23:07 -0500

    Add files via upload

---
+++ bool.erl
@@ -0,0 +1,11 @@
+-module(bool).
+-export([b_not/1,b_and/2,b_or/2,b_xor/2]).
+b_not(false)->true;
+b_not(true)->false.
+b_and(true,true)->true;
+b_and(_,_)->false.
+b_or(false,false)->false;
+b_or(_,_)->true.
+b_xor(false,false)->false;
+b_xor(true,true)->false;
+b_xor(_,_)->true.
